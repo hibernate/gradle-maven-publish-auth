ed4bb067bebed38bdb0e26d37aaad7740556588a
DNAlchemist <DNAlchemist@ya.ru>
2019-12-19 00:16:20 +0300

    add license

--- src/test/java/org/hibernate/build/publish/auth/maven/pwd/PropertyMapTest.java
+++ src/test/java/org/hibernate/build/publish/auth/maven/pwd/PropertyMapTest.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
 package org.hibernate.build.publish.auth.maven.pwd;
 
 import org.junit.Test;
@@ -6,6 +12,9 @@ import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
+/**
+ * @author Ruslan Mikhalev
+ */
 public class PropertyMapTest {
 
     @Test
