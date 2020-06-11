00b4edaed6c1616eb0ba0cd33499dae5f23f9a98
Paul Scholz <paul.scholz@scholzi100.de>
2020-06-11 23:53:23 +0200

    Added newline to end of SimplePropertyMapTest, added JavaDoc

--- src/main/java/org/hibernate/build/publish/auth/maven/pwd/PropertyMap.java
+++ src/main/java/org/hibernate/build/publish/auth/maven/pwd/PropertyMap.java
@@ -8,6 +8,19 @@ package org.hibernate.build.publish.auth.maven.pwd;
 
 import java.util.Optional;
 
+/**
+ * Contract of collecting properties to be replaced in settings.xml.
+ */
 public interface PropertyMap {
+
+    /**
+     * Retrieve value to a key
+     *
+     * @param key property key
+     * @return Optional is empty if key could not be found
+     * @see Optional#isPresent()
+     * @see Optional#get()
+     */
     Optional<String> get(String key);
+
 }
